e7acc31fc73df4a496ffb2d639675fc57e007a1f
tyler <tyler@cruftbusters.com>
2021-03-26 23:33:34 -0500

    render triangle

--- stl.py
+++ stl.py
@@ -4,3 +4,13 @@ def render(string):
 
 def vertex(x, y, z):
     return f"vertex {x} {y} {z}"
+
+
+def triangle(v1, v2, v3):
+    return f"""facet normal 0 0 0
+  outer loop
+    {v1}
+    {v2}
+    {v3}
+  endloop
+endfacet"""
--- test_stl.py
+++ test_stl.py
@@ -7,3 +7,19 @@ def test_render_vertex():
 
 def test_vertex():
     assert vertex(1, 2, 3) != vertex(3, 2, 1)
+
+
+def test_render_triangle():
+    assert render(
+        triangle(
+            vertex(1, 2, 3),
+            vertex(4, 5, 6),
+            vertex(7, 8, 9),
+        ),
+    ) == """facet normal 0 0 0
+  outer loop
+    vertex 1 2 3
+    vertex 4 5 6
+    vertex 7 8 9
+  endloop
+endfacet"""
