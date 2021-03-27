21287ba18cfd5e5bf0282cc24a16502b14309f0c
tyler <tyler@cruftbusters.com>
2021-03-27 00:49:20 -0500

    subdivide one side of triangle prism

--- generate.py
+++ generate.py
@@ -1,16 +1,20 @@
 from stl import *
 from argparse import ArgumentParser
+from math import sqrt
 
 parser = ArgumentParser()
 parser.add_argument("destination")
 args = parser.parse_args()
 
+radius = sqrt(3) * 2 / 3
+height = 0.25
+
 facets = render(
     subdivide(
-        vertex(-1, 0, 0),
-        vertex(1, 0, 0),
-        vertex(1, 1, 0),
-        vertex(-1, 1, 0),
+        polarVertex(radius, 0, -height / 2),
+        polarVertex(radius, 120, -height / 2),
+        polarVertex(radius, 120, height / 2),
+        polarVertex(radius, 0, height / 2),
         72,
     ),
 )
