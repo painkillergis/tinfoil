752fcfb196df004754a729210fc0b9bbbcb7318d
tyler <tyler@cruftbusters.com>
2021-04-24 16:16:06 -0500

    tri model from heightmap uses solid type

--- triModelFromHeightmap.py
+++ triModelFromHeightmap.py
@@ -10,30 +10,33 @@ args = parser.parse_args()
 radius = sqrt(3) * 2 / 3
 height = 0.25
 
-facets = render(
-    [
-        ladderSubdivideQuads(
-            polarVertex(radius, t, -height / 2),
-            polarVertex(radius, t + 120, -height / 2),
-            polarVertex(radius, t + 120, height / 2),
-            polarVertex(radius, t, height / 2),
-            72,
-        ) for t in range(0, 360, 120)
-    ],
-    triangle(*[polarVertex(radius, t, -height / 2) for t in range(0, 360, 120)]),
-    trianglesFromSubdivisionPoints(
-        10,
-        sampleZ(
-            args.source,
-            subdividePoints(
+contents = render(
+    solid(
+        "sample",
+        [
+            [
+                ladderSubdivideQuads(
+                    polarVertex(radius, t, -height / 2),
+                    polarVertex(radius, t + 120, -height / 2),
+                    polarVertex(radius, t + 120, height / 2),
+                    polarVertex(radius, t, height / 2),
+                    72,
+                ) for t in range(0, 360, 120)
+            ],
+            triangle(*[polarVertex(radius, t, -height / 2) for t in range(0, 360, 120)]),
+            trianglesFromSubdivisionPoints(
                 10,
-                *[polarVertex(radius, t, height / 2) for t in range(0, 360, 120)],
+                sampleZ(
+                    args.source,
+                    subdividePoints(
+                        10,
+                        *[polarVertex(radius, t, height / 2) for t in range(0, 360, 120)],
+                    ),
+                )
             ),
-        )
+        ]
     )
 )
 
 with open(args.destination, "w") as f:
-    f.write(f"""solid sample
-{facets}
-endsolid sample""")
+    f.write(contents)
